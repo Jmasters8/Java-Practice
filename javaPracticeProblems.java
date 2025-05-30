6b841cbee3469c25e8519879920727d53ce17a32
jason <jmasters1000@yahoo.com>
2025-05-29 22:57:55 -0700

    Add problem 2

--- javaPracticeProblems.java
+++ javaPracticeProblems.java
@@ -5,4 +5,16 @@ public boolean sleepIn(boolean weekday, boolean vacation) {
 
   return false;
 
-  
+
+
+
+//Given a string and a non-negative int n,
+//return a larger string that is n copies of the original string.
+public String stringTimes(String str, int n) {
+  String result = "";
+  for (int i=0; i<n; i++) {
+    result = result + str;  // could use += here
+  }
+  return result;
+}
+
